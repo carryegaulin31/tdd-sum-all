1bfced8b18511f82b51a03c719dcd7807c4d2faa
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-16 19:45:56 -0400

    require creates and first test written

---
+++ tests/sum-all.test.js
@@ -0,0 +1,20 @@
+var describe = require('mocha').describe
+var it = require('mocha').it
+var sumAll = require('../calculation')
+var expect = require('chai').expect
+
+describe('calculation', () => {
+  describe('sumAll', () => {
+    it('retutn the sum of all the numbers in the array', () => {
+      //set up test data
+      var testData = [42, 76, 100]
+      
+      //call the function from outside file
+      var total = sumAll(testData)
+      
+      //assert what should be true
+      expect(total).to.equal(218)
+    })
+  })
+
+})
