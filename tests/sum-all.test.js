5cbabc357c38f3ea75d74e28b7f3409bd72b42fc
Carrye Gaulin <carryegaulin033178@gmail.com>
2021-03-16 20:56:51 -0400

    disabled eslint for describe and it issue

--- tests/sum-all.test.js
+++ tests/sum-all.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // var describe = require('mocha').describe
 // var it = require('mocha').it
 var sumAll = require('../calculation')
@@ -9,13 +10,12 @@ describe('calculation', () => {
     it('retutn the sum of all the numbers in the array', () => {
       //set up test data
       var testData = [42, 76, 100]
-      
+
       //call the function from outside file
       var total = sumAll(testData)
-      
+
       //assert what should be true
       expect(total).to.equal(218)
     })
   })
-
 })
